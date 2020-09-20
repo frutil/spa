5e5bcc37dd37f79ff00774d205913ad628a94178
Witoslaw Koczewski <wi@koczewski.de>
2020-09-20 16:28:51 +0200

    mui

--- src/frutil/spa/mui.cljs
+++ src/frutil/spa/mui.cljs
@@ -1,6 +1,31 @@
 (ns frutil.spa.mui
   (:require
-   [cljs.pprint :as pprint]))
+   [cljs.pprint :as pprint]
+
+   [reagent.core :as r]
+   [reagent.dom :as rdom]
+
+   [reagent-material-ui.core.css-baseline :refer [css-baseline]]
+   [reagent-material-ui.styles :as styles]
+   [reagent-material-ui.core.grid :refer [grid]]
+   [reagent-material-ui.core.card :refer [card]]
+   [reagent-material-ui.core.card-content :refer [card-content]]))
+
+
+;;; JSS
+
+(defn styled-component [styles component]
+  (let [component-styles (fn [theme]
+                           {:style (styles theme)})
+        with-custom-styles (styles/with-styles component-styles)
+        StyledComponent (fn [{:keys [classes] :as _props}]
+                          [:div
+                           {:class (:style classes)}
+                           component])]
+    [(with-custom-styles StyledComponent)]))
+
+
+;;;
 
 
 (defn Data
@@ -18,3 +43,64 @@
              (catch :default ex
                "!!! ERROR !!! pprint failed for data"))])
         datas)))
+
+;;; layouts
+
+(defn Stack [options & components]
+  (into
+   [grid
+    (assoc options
+           :container true
+           :direction :column
+           :spacing (get options :spacing 1))]
+   (map (fn [component]
+          [grid
+           {:item true}
+           component])
+        components)))
+
+;;;
+
+(defn Card [& children]
+  [card
+   [card-content
+    (into
+     [grid {:container true :direction :column :spacing 1}]
+     (map (fn [child]
+            [grid {:item true}
+             child])
+          children))]])
+
+
+;;;
+
+;;; dialogs
+
+
+(defonce ACTIVE_DIALOG (r/atom nil))
+
+
+(defn DialogsContainer []
+  [:div.DialogsContainer
+   (when-let [dialog @ACTIVE_DIALOG]
+     (conj dialog
+           #(reset! ACTIVE_DIALOG nil)))])
+
+
+(defn show-dialog [dialog]
+  (reset! ACTIVE_DIALOG dialog))
+
+
+;;; app
+
+
+(defn app [custom-theme custom-styles Content]
+  [:<> ; fragment
+   [css-baseline]
+   [styles/theme-provider (styles/create-mui-theme custom-theme)
+    (styled-component custom-styles [Content])]])
+
+
+(defn mount-app [custom-theme custom-styles Content]
+  (rdom/render (app custom-theme custom-styles Content)
+               (js/document.getElementById "app")))
