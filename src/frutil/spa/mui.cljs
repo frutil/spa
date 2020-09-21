7d4c4aceb62dd299e3407fdc8d63fea4b85239f6
Witoslaw Koczewski <wi@koczewski.de>
2020-09-21 19:57:55 +0200

    mui: ErrorBoundary

--- src/frutil/spa/mui.cljs
+++ src/frutil/spa/mui.cljs
@@ -11,7 +11,9 @@
    [reagent-material-ui.core.typography :refer [typography]]
    [reagent-material-ui.core.grid :refer [grid]]
    [reagent-material-ui.core.card :refer [card]]
-   [reagent-material-ui.core.card-content :refer [card-content]]))
+   [reagent-material-ui.core.card-content :refer [card-content]]
+
+   [reagent-material-ui.icons.bug-report :refer [bug-report]]))
 
 
 ;;; JSS and Theme
@@ -52,6 +54,135 @@
              (catch :default ex
                "!!! ERROR !!! pprint failed for data"))])
         datas)))
+
+
+(defn HTML [html-code]
+  [:div.HTML
+   {:dangerouslySetInnerHTML {:__html html-code}}])
+
+
+;;; exceptions
+
+
+(defn- stack->hiccup [stack]
+  (reduce (fn [div line]
+            (conj div
+                  [:div
+                   {:style {:color (when (or
+                                          (-> line
+                                              (str/includes? "cljs.core.js"))
+                                          (-> line
+                                              (str/includes? "react_dom_development.js"))
+                                          (-> line
+                                              (str/includes? "reagent.ratom.js"))
+                                          (-> line
+                                              (str/includes? "reagent.dom.js"))
+                                          (-> line
+                                              (str/includes? "reagent.impl.component.js")))
+                                     "#aaa")}}
+                   (-> line
+                       (str/replace #"@" " @ ")
+                       (str/replace #"\$" " "))]))
+          [:div]
+          (-> stack (.split "\n"))))
+
+
+(defn StackTrace [stack]
+  (when stack
+    [:div
+     {:style {:font-family :monospace
+              :white-space :pre-wrap
+              :padding "1rem 0"}}
+     (stack->hiccup
+      (if (-> stack (.startsWith "\n"))
+        (-> stack (.substring 1))
+        stack))]))
+
+
+(defn Exception [exception]
+  (let [message (.-message exception)
+        message (if message message (str exception))
+        data (ex-data exception)
+        cause (or (ex-cause exception) (.-cause ^js exception))
+        stack (.-stack exception)]
+    [:div.Exception
+     (when cause
+       [:div
+        [Exception cause]
+        [:div
+         {:style {:margin "1rem 0"
+                  :color :lightgrey
+                  :font-style :italic}}
+         "causes"]])
+     ;; [:> mui/Card
+     ;;  (js/console.log exception)
+     ;;  [Data (-> exception)]]
+     [:div
+      {:style {:font-weight :bold}}
+      (str message)]
+     [StackTrace stack]
+     (when-not (empty? data)
+       (if (= ::error-boundary (-> data :error-type))
+         [:div
+          [StackTrace (get-in data [:info "componentStack"])]
+          [Data (update data :info :dissoc "componentStack")]]
+         [Data data]))]))
+
+
+(defn ErrorCard [& contents]
+  [card
+   {:style {:background-color "#b71c1c" ; red 900
+            :color "#ffffff"
+            :min-width "400px"}}
+   [card-content
+    [:div
+     {:style {:display :flex
+              :overflow-x :auto}}
+     [:> bug-report
+      {:style {:margin-right "1rem"}}]
+     (into [:div] contents)]]])
+
+
+(defn ExceptionCard [exception]
+  [ErrorCard
+    (if exception
+      [:div
+       {:style {:margin "1em"}}
+       [Exception exception]]
+      [:div "A bug is making trouble :-("])])
+
+
+(defn ErrorBoundary [comp]
+  (if-not comp
+    [:span]
+    (let [!exception (r/atom nil)]
+      (r/create-class
+       {:component-did-catch
+        (fn [this ex info]
+          (let [cljs-react-class (-> comp first .-cljsReactClass)
+                comp-name (if cljs-react-class
+                            (-> cljs-react-class .-displayName)
+                            "no-name")]
+            (js/console.log
+             "ErrorBoundary"
+             "\nthis:" this
+             "\ncomp" comp
+             "\ncomp-name" comp-name
+             "\nex:" ex
+             "\ninfo:" info)
+            (reset! !exception (ex-info (str "Broken component: " comp-name)
+                                        {:error-type ::error-boundary
+                                         :component comp
+                                         :info (js->clj info)
+                                         :react-class cljs-react-class}
+                                        ex))))
+        :reagent-render (fn [comp]
+                          (if-let [exception @!exception]
+                            [ExceptionCard exception]
+                            comp))}))))
+
+
+
 
 ;;; typography
 
